7f9640dc0a9533cbc0dceb0038e171f217d0e8c5
johan <stjernholm@msn.com>
2020-04-04 23:21:35 +0800

    Feature: remove from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,6 +33,14 @@ export const removeExpense = ({ id } = {}) => ({
   id
 })
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }))
+    })
+  }
+}
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -50,7 +58,7 @@ export const startSetExpenses = () => {
   return (dispatch) => {
     return database.ref('expenses').once('value').then((snapshot) => {
       const expenses = []
-      
+
       snapshot.forEach((childSnapshot) => {
         expenses.push({
           id: childSnapshot.key,

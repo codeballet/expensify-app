c7d158676f5585653bcbfbe11241ec1011aa0162
johan <stjernholm@msn.com>
2020-04-06 12:09:31 +0800

    Feature: add startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -6,6 +6,7 @@ import {
   editExpense,
   removeExpense,
   setExpenses,
+  startEditExpense,
   startSetExpenses,
   startRemoveExpense
 } from '../../actions/expenses'
@@ -54,6 +55,31 @@ test('should setup editExpense action object', () => {
     type: 'EDIT_EXPENSE',
     id: '123abc',
     updates: { note: 'New note value' }
+  })
+})
+
+test('should edit expense from firebase', (done) => {
+  const store = createMockStore({})
+  const id = expenses[1].id
+  const updates = {
+    description: 'Screen',
+    amount: 5000,
+    note: 'IPS type',
+    createdAt: 2000
+  }
+
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    })
+
+    return database.ref(`expenses/${id}`).once('value')
+  }).then((snapshot) => {
+    expect(snapshot.val()).toEqual(updates)
+    done()
   })
 })
 

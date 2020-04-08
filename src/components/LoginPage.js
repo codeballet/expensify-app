6d3b7ba9ac6533a865d235acdef8b73d198978fb
johan <stjernholm@msn.com>
2020-04-08 15:36:03 +0800

    Feat: add login styles, remove dependencies style-loader uuid validator

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,12 @@ import { startLogin } from '../actions/auth'
 
 export const LoginPage = ({ startLogin }) => {
   return (
-    <div>
-      <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1 className="box-layout__title">Espensify App</h1>
+        <p>Get your expenses under control</p>
+        <button onClick={startLogin}>Login</button>
+      </div>
     </div>
   )
 }

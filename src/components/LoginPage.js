15468dbd3543cb041019e3b009e510a1358d2856
johan <stjernholm@msn.com>
2020-04-08 17:33:47 +0800

    Feat: style login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,8 +6,8 @@ export const LoginPage = ({ startLogin }) => {
   return (
     <div className="box-layout">
       <div className="box-layout__box">
-        <h1 className="box-layout__title">Espensify App</h1>
-        <p>Get your expenses under control</p>
+        <h1 className="box-layout__title">Expensify</h1>
+        <p>Control your expenses</p>
         <button onClick={startLogin}>Login</button>
       </div>
     </div>
